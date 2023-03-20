23a253b16fe4851d5003b2902a0d8056354e899d
JoonasMattinen <N2majo02.students.oamk.fi>
2023-03-20 10:34:36 +0200

    added functions and headers

--- inc/headers.php
+++ inc/headers.php
@@ -1,12 +1,17 @@
 <?php
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Accept, Content-Type, Access-Control-Allow-Header');
+header('Content-Type: application/json');
+header('Access-Control-Max-Age: 3600');
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credential: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS,DELETE");
-header("Access-Control-Allow-Headers: Content-type");
-header("Access-Control-Max-Age: 3600");
-header("Content-Type: application/json");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
 
-if ($_SERVER["REQUEST_METHOD"]=== "OPTIONS") {
-    return 0;
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  
+  exit(0);
 }
