12ecb50b9a7901165b72fdf34e7087e63e77f8f0
JoonasMattinen <N2majo02.students.oamk.fi>
2023-03-21 13:12:07 +0200

    added php that fetches product with certain id
    Co-authored-by: matiyp <matiyp@users.noreply.github.com>
    Co-authored-by: R-M. Isola <oamk-isri@users.noreply.github.com>
    Co-authored-by: Rikhardson <Rikhardson@users.noreply.github.com>

--- products/getproduct.php
+++ products/getproduct.php
@@ -1,1 +1,19 @@
 <?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+// localhost:3000/products/getproduct.php/test1/test2/test3
+$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+
+$parameters = explode('/',$uri);
+$product_id = $parameters[0];
+
+
+
+try {
+  $db = openDb();
+  selectRowAsJson($db,"select * from product where productid = $product_id");
+}
+catch (PDOException $pdoex) {
+  returnError($pdoex);
+}
