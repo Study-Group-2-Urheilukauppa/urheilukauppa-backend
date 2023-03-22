64e176c5dfacdb282e9928b514e3ac3e5f908d59
JoonasMattinen <N2majo02.students.oamk.fi>
2023-03-22 11:09:57 +0200

    add update.php

---
+++ update.php
@@ -0,0 +1,23 @@
+<?php
+// This is only example from another practice
+require_once "inc/functions.php";
+require_once "inc/headers.php";
+
+$input = json_decode(file_get_contents("php://input"));
+$id = filter_var($input->id,FILTER_SANITIZE_STRING);
+$description = filter_var($input->description,FILTER_SANITIZE_STRING);
+
+try {
+    $db = openDb();
+
+    $query = $db->prepare("update task set description=:description where id=:id");
+    $query->bindValue(":description",$description,PDO::PARAM_STR);
+    $query->bindValue(":id",$id,PDO::PARAM_INT);
+    $query->execute();
+
+    header("HTTP/1.1 200 OK");
+    $data = array("id" => $id,"description" => $description);
+    print json_encode($data);
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
