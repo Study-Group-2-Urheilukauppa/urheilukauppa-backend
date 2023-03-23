659107f1bd8c5f7432622715e2ff04636f820576
JoonasMattinen <N2majo02.students.oamk.fi>
2023-03-23 11:28:37 +0200

    added .php file to add new categories

--- addCategory.php
+++ addCategory.php
@@ -5,16 +5,16 @@ require_once "inc/functions.php";
 require_once "inc/headers.php";
 
 $input = json_decode(file_get_contents("php://input"));
-$description = filter_var($input->description,FILTER_SANITIZE_STRING);
+$categoryname = filter_var($input->categoryname,FILTER_SANITIZE_STRING);
 
 try {
     $db = openDb();
-    $query = $db->prepare("insert into task(description) values (:description)");
-    $query->bindValue(":description", $description,PDO::PARAM_STR);
+    $query = $db->prepare("insert into category(categoryname) values (categoryname)");
+    $query->bindValue(":categoryname", $categoryname,PDO::PARAM_STR);
     $query->execute();
 
     header("HTTP/1.1 200 OK");
-    $data = array("id" => $db->lastInsertId(),"description" => $description);
+    $data = array("categoryid" => $db->lastInsertId(),"categoryname" => $categoryname);
     print json_encode($data);
 } catch (PDOException $pdoex) {
     returnError($pdoex);
