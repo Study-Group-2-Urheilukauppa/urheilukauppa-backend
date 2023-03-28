73b23c28a772498f2dd939efec97ab5e956539e4
matiyp <n2ylma00@students.oamk.fi>
2023-03-28 14:36:49 +0300

    Created Feedback.php

---
+++ Feedback.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "inc/functions.php";
+require_once "inc/headers.php";
+// Get the form data from the request body
+$data = json_decode(file_get_contents('php://input'));
+
+// Validate the form data
+if (!isset($data->name) || !isset($data->email) || !isset($data->feedback)) {
+  header('HTTP/1.1 400 Bad Request');
+  $error = array('error' => 'Missing required form data.');
+  echo json_encode($error);
+  exit;
+}
+
+// Sanitize the form data
+$name = filter_var($data->name, FILTER_SANITIZE_STRING);
+$email = filter_var($data->email, FILTER_SANITIZE_EMAIL);
+$text = filter_var($data->feedback, FILTER_SANITIZE_STRING);
+
+try {
+  $db = openDB();
+
+  // Prepare the SQL query to insert the feedback data into the database
+  $sql = "INSERT INTO Feedback (name, email, text) VALUES ('$name', '$email', '$text')";
+
+  // Execute the SQL query and get the ID of the inserted row
+  $id = executeInsert($db, $sql);
+
+  // Send a success response back to the frontend
+  header('Content-Type: application/json');
+  echo json_encode(['success' => true, 'id' => $id]);
+
+} catch (PDOException $pdoex) {
+  // If there was an error inserting the data, return an error response
+  returnError($pdoex);
+}
