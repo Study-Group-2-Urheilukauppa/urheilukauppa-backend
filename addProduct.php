26bf150e1e7aa9d0a5e68e95e78ab383df79cb5b
Rikhardson <n2puri00@students.oamk.fi>
2023-04-03 15:29:00 +0300

    Added fulldescript to addProduct

--- addProduct.php
+++ addProduct.php
@@ -10,22 +10,24 @@ $price = filter_var($input->price, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALL
 $sale = filter_var($input->sale, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 $imgURL = filter_var($input->imgURL, FILTER_SANITIZE_STRING);
 $descript = filter_var($input->descript, FILTER_SANITIZE_STRING);
+$fulldescript = filter_var($input->fulldescript, FILTER_SANITIZE_STRING);
 
 try {
     $db = openDb();
-    $query = $db->prepare("INSERT INTO Product(productname, categoryid, price, sale, imgURL, descript) VALUES
-         (:productname, :categoryid, :price, :sale, :imgURL, :descript)");
+    $query = $db->prepare("INSERT INTO Product(productname, categoryid, price, sale, imgURL, descript, fulldescript) VALUES
+         (:productname, :categoryid, :price, :sale, :imgURL, :descript, :fulldescript)");
     $query->bindValue(":productname", $productname, PDO::PARAM_STR);
     $query->bindValue(":categoryid", $categoryid, PDO::PARAM_STR);
     $query->bindValue(":price", $price, PDO::PARAM_STR);
     $query->bindValue(":sale", $sale, PDO::PARAM_STR);
     $query->bindValue(":imgURL", $imgURL, PDO::PARAM_STR);
     $query->bindValue(":descript", $descript, PDO::PARAM_STR);
+    $query->bindValue(":fulldescript", $fulldescript, PDO::PARAM_STR);
     $query->execute();
 
     header("HTTP/1.1 200 OK");
     $data = array("productid" => $db->lastInsertId(), "productname" => $productname, "categoryid" => $categoryid,
-             "price" => $price, "sale" => $sale, "imgURL" => $imgURL, "descript" => $descript);
+             "price" => $price, "sale" => $sale, "imgURL" => $imgURL, "descript" => $descript, "fulldescript" => $fulldescript);
     print json_encode($data);
 } catch (PDOException $pdoex) {
     returnError($pdoex);
