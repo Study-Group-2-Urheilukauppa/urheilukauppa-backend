fa595a3825aed5d19f4a4541f5f8e50a1312bb4a
JoonasMattinen <N2majo02.students.oamk.fi>
2023-04-04 15:16:37 +0300

    login test

--- login.php
+++ login.php
@@ -1,26 +1,49 @@
 <?php
-
-require_once "inc/functions.php";
-require_once 'inc/headers.php';
-// Get the login data from the POST request
-$username = $_POST['username'];
-$password = $_POST['password'];
-$hashedPw = password_hash($password, PASSWORD_DEFAULT);
-
-
-$db = openDB();
-$command = 'SELECT * FROM adminlogin WHERE username = :? AND pw = :?';
-$stmt = $db->prepare($command);
-$stmt->execute(array($username, $hashedPw));
-
-if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    // The credentials are valid, set the response status to success
-    $response = array('success' => true, $username => $row['username'], $hashedPw => $row['pw']);
-    echo json_encode($response);
-} else {
-    // The credentials are invalid, set the response status to failure and provide an error message
-    $response = array('success' => false, 'message' => 'Invalid username or password');
-    echo json_encode($response);
+require_once './inc/functions.php';
+require_once './inc/headers.php'; 
+$data = json_decode(file_get_contents("php://input"));
+    $username = htmlspecialchars($data->username);
+    $password = htmlspecialchars(password_hash($data->password, PASSWORD_DEFAULT));
+    
+$dbcon = openDB();
+try {
+    $dbcon;
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
 }
 
+// Prepare and execute the SQL statement to retrieve user credentials
+$stmt = $dbcon->prepare("SELECT * FROM Login WHERE username=:username LIMIT 1");
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
 
+// Check if a user was found with the provided username
+if ($user !== false) {
+    // Verify the password using the PHP password_verify function
+    if (password_verify($password, $user['password'])) {
+        // Return a success response
+        $response = [
+            'success' => true
+        ];
+    } else {
+        // Return an error response
+        $response = [
+            'success' => false,
+            'message' => 'Invalid password'
+        ];
+    }
+} else {
+    // Return an error response
+    $response = [
+        'success' => false,
+        'message' => 'Invalid username'
+    ];
+}
+
+// Close the database connection
+$conn = null;
+
+// Return the response as JSON
+header('Content-Type: application/json');
+echo json_encode($response);
