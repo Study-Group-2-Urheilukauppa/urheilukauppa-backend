faec1ee9060be27161113b44115d5c7b1679a0db
Rikhardson <n2puri00@students.oamk.fi>
2023-04-18 10:10:48 +0300

    amount change added

--- products/allproducts.php
+++ products/allproducts.php
@@ -2,19 +2,47 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
-// localhost:3000/products/getproduct.php/1001
-$url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$path = parse_url($url, PHP_URL_PATH);
-$parts = explode('/', $path);
-$product_id = $parts[count($parts)-1];
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the product data from the request body
+    $data = json_decode(file_get_contents('php://input'), true);
+    
+    // Extract the product ID and amount from the data
+    $product_id = $data['productid'];
+    $new_amount = $data['amount'];
 
-try {
-    $db = openDb();
-    $sql = "SELECT Product.productid, Product.productname, Product.price, Product.sale, Product.categoryid, Product.amount, Category.categoryname FROM Product INNER JOIN Category ON Product.categoryid=Category.categoryid;";
-    $query = $db->query($sql);
-    $results = $query->fetchALL(PDO::FETCH_ASSOC);
-    header("HTTP/1.1 200 OK");
-    print json_encode($results);
-} catch (PDOException $pdoex) {
-    returnError($pdoex);
+    try {
+        // Open the database connection
+        $db = openDb();
+        
+        // Prepare the SQL query to update the product amount
+        $stmt = $db->prepare("UPDATE Product SET amount = :amount WHERE productid = :productid");
+        $stmt->bindValue(':amount', $new_amount, PDO::PARAM_INT);
+        $stmt->bindValue(':productid', $product_id, PDO::PARAM_INT);
+        
+        // Execute the query
+        $stmt->execute();
+        
+        // Return a success response
+        header("HTTP/1.1 200 OK");
+        header('Content-Type: application/json');
+        echo json_encode(array('message' => 'Product amount updated successfully'));
+
+    } catch (PDOException $pdoex) {
+        // Return an error response
+        returnError($pdoex);
+    }
+} else {
+    // If the request method is not POST, return the list of products
+    try {
+        $db = openDb();
+        $sql = "SELECT Product.productid, Product.productname, Product.price, Product.sale, Product.categoryid, Product.amount, Category.categoryname FROM Product INNER JOIN Category ON Product.categoryid=Category.categoryid;";
+        $query = $db->query($sql);
+        $results = $query->fetchALL(PDO::FETCH_ASSOC);
+        header("HTTP/1.1 200 OK");
+        header('Content-Type: application/json');
+        print json_encode($results);
+    } catch (PDOException $pdoex) {
+        returnError($pdoex);
+    }
 }
