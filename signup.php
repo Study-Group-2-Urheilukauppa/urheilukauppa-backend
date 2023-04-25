c6f1d973ae92ec2d6720ff0476b01d7c87c7e6ce
JoonasMattinen <N2majo02.students.oamk.fi>
2023-04-25 13:46:38 +0300

    connected client and login tables

--- signup.php
+++ signup.php
@@ -1,20 +1,31 @@
 <?php
-    require_once './inc/functions.php';
-    require_once './inc/headers.php'; 
-    
+require_once './inc/functions.php';
+require_once './inc/headers.php'; 
 
-    // Get user data from frontend
-    $data = json_decode(file_get_contents("php://input"));
-    $username = htmlspecialchars($data->username);
-    $password = htmlspecialchars(password_hash($data->password, PASSWORD_DEFAULT));
+// Get user data from frontend
+$data = json_decode(file_get_contents("php://input"));
+$firstname = $data->firstname;
+$lastname = $data->lastname;
+$postal = $data->postal;
+$address = $data->address;
+$phone = $data->phone;
+$username = htmlspecialchars($data->username);
+$password = htmlspecialchars(password_hash($data->password, PASSWORD_DEFAULT));
 
-    try{
-    $dbcon = openDB();
-    $sql = "INSERT INTO Login (Username, Pw) VALUES (?, ?)";
+try {
+  $dbcon = openDB();
 
-    // Insert user data into database
-    $statement = $dbcon->prepare($sql);
-    $statement->execute([$username, $password]);
-    }catch(PDOException $pdoex){
-      returnError($pdoex);
-    }
+  // Insert client data
+  $sql = "INSERT INTO Client (Firstname, Lastname, Postalcode, Clientaddress, Phonenumber) VALUES (?, ?, ?, ?, ?)";
+  $statement = $dbcon->prepare($sql);
+  $statement->execute([$firstname, $lastname, $postal, $address, $phone]);
+  $clientid = $dbcon->lastInsertId();
+
+  // Insert login data
+  $sql = "INSERT INTO Login (clientid, Username, Pw) VALUES (?, ?, ?)";
+  $statement = $dbcon->prepare($sql);
+  $statement->execute([$clientid, $username, $password]);
+
+} catch(PDOException $pdoex) {
+  returnError($pdoex);
+}
