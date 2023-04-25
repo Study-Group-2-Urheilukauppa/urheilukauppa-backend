35b41c00f8826ce1d4fe004c684db07fb386e7c2
JoonasMattinen <N2majo02.students.oamk.fi>
2023-04-25 11:13:56 +0300

    added orderrow

--- viewOrders.php
+++ viewOrders.php
@@ -5,7 +5,7 @@ require_once './inc/headers.php';
 
 try {
   $db = openDb();
-  selectAsJson($db,'select * from orders');
+  selectAsJson($db,'select * from Orders,Orderrow where Orders.ordernum = Orderrow.ordernum');
 }
 catch (PDOException $pdoex) {
   returnError($pdoex);
