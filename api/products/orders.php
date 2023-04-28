c930f5ab7196ddcd4db538d05ac7d7bf770088f0
JoonasMattinen <N2majo02.students.oamk.fi>
2023-04-28 21:40:40 +0300

    modified foreach loop

--- api/products/orders.php
+++ api/products/orders.php
@@ -8,32 +8,44 @@ $cart = $data->cart;
 
 try {
     $dbcon = openDB();
-  
+
+    // Start transaction
+    $dbcon->beginTransaction();
+
     // Insert client data
     $sql = "INSERT INTO Orders (clientid) VALUES (?)";
     $statement = $dbcon->prepare($sql);
     $statement->execute([$userId]);
     $clientid = $dbcon->lastInsertId();
-  
+
     // Insert product data
-    $sql = "INSERT INTO Orderrow (ordernum, productid, pcs) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO Orderrow (ordernum, rownum, productid, pcs) VALUES (?, ?, ?, ?)";
     $statement = $dbcon->prepare($sql);
+
+    $rownum = 1; // Initialize rownum to 1
 
     foreach ($cart as $productid => $item) {
         $amount = $item->amount;
-        
-        $statement->execute([$clientid, $productid, $amount]);
+
+        $statement->execute([$clientid, $rownum, $productid, $amount]);
+
+        $rownum++; // Increment rownum by 1
     }
 
-    
+    // Commit transaction
+    $dbcon->commit();
 
     $response = array(
         "orderid" => $clientid,
-        "message" => "Order placed successfully"
+        "success" => "Order placed successfully"
     );
 
     echo json_encode($response);
-  
+
 } catch(PDOException $pdoex) {
+    // Rollback transaction
+    $dbcon->rollback();
+
     returnError($pdoex);
 }
+
