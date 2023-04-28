4e2dd23321e7cc20add9b42a537db70a838757e0
JoonasMattinen <N2majo02.students.oamk.fi>
2023-04-28 14:26:11 +0300

    added INSERT

--- api/products/orders.php
+++ api/products/orders.php
@@ -1,3 +1,36 @@
 <?php
-require_once '../inc/functions.php';
-require_once '../inc/headers.php';
+
+$data = json_decode(file_get_contents("php://input"));
+$userId = $data->userid;
+$cart = $data->cart;
+
+try {
+    $dbcon = openDB();
+  
+    // Insert client data
+    $sql = "INSERT INTO Orders (clientid) VALUES (?)";
+    $statement = $dbcon->prepare($sql);
+    $statement->execute([$userId]);
+    $clientid = $dbcon->lastInsertId();
+  
+    // Insert product data
+    $sql = "INSERT INTO Orderrow (orderid, productid, amount) VALUES (?, ?, ?)";
+    $statement = $dbcon->prepare($sql);
+    
+    foreach ($cart as $item) {
+        $productid = $item->productid;
+        $amount = $item->amount;
+        
+        $statement->execute([$clientid, $productid, $amount]);
+    }
+
+    $response = array(
+        "orderid" => $clientid,
+        "message" => "Order placed successfully"
+    );
+
+    echo json_encode($response);
+  
+} catch(PDOException $pdoex) {
+    returnError($pdoex);
+}
